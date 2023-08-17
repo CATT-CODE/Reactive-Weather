7de522fdad24891f5e57721458c7f2a6894835f0
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-08-17 18:52:02 -0400

    Day 1

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import WeatherCard from './components/WeatherCard';
+import cities from './data';
 // Import data and WeatherCard here
+
 
 
 function App() {
@@ -7,12 +10,12 @@ function App() {
         <>
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
+        {cities.map((city, index) => (
             <div className = "app">
-                {/* Render components here */}
-                
-
+                <WeatherCard key={index} city={city.city} temperature={city.temperature} forecast={city.forecast} />
 
             </div>
+        ))}
             
         </>
     )
